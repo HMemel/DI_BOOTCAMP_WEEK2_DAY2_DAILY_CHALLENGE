269bff2f8b7354bd1a89f902c75ddbd2a3e037c5
Martin Memel <memelmegame@gmail.com>
2023-01-10 18:50:22 +0000

    first commit

--- exercice.js
+++ exercice.js
@@ -1,17 +1,19 @@
 // Daily Challenge: Not Bad
 
 // Create a variable called . The value of the variable should be a string that contains the words “not” and “bad”.
-const sentence = "The film is not so bad, I like it";
+let strings = "The film is not so bad, I like it";
+let array = strings.split("");
 
 // Create a variable called where it’s value is the first appearance (ie. the position) of the substring “not” (from the variable).
-const wordNot = sentence.indexOf("not");
+let wordNot = strings.indexOf("not");
 
 // Create a variable called where it’s value is the first appearance (ie. the position) of the substring “bad” (from the variable).
-const wordBad = sentence.indexOf("bad");
+const wordBad = strings.indexOf("bad");
 
 // If the word “bad” comes after the word “not”, you should replace the whole “not…bad” substring with “good”, then console.log the result.
-if (wordNot !== -1 && wordBad !== -1 && wordNot < wordBad) {
-  const newSentence = sentence.slice(0, wordNot) + "good" + sentence.slice(wordBad + 3);
+if (wordNot < wordBad && wordBad !== -1) {
+  array.splice(wordNot, wordBad-wordNot+3, "good");
+  newSentence = array.join("");
   console.log(newSentence);
 
 // If the word “bad” does not come after “not” or the words are not in the sentence, console.log the original sentence.
